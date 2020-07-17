f3ca53846b3ed8e1ba2d004e0bbe1c71629361a7
Leonardo V Oliveira <leonardo.viegaso@gmail.com>
2020-07-16 23:51:06 -0300

    aprendendo comandos

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Leonardo Viegas";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo.";
+
+//alert("Bem vindo " + nome + ", voce tem " + idade + " anos");
+console.log(nome);
+console.log(idade + idade2);
+console.log(idade * idade2);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toLowerCase);
+console.log(frase.toUpperCase);
+//alert(frase.replace("Japão", "Brasil"));
