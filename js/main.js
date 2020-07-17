ffc7af1f1baad299b519df39f1e3fc5523ad7aab
Leonardo V Oliveira <leonardo.viegaso@gmail.com>
2020-07-17 13:00:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
-var nome = "Leonardo Viegas";
-var idade = 29;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo.";
+//var nome = "Leonardo Viegas";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo.";
+//var lista = ["maça", "pera", "laranja"];
+//var fruta = {nome:"maça", cor:"vermelha"};
+//var frutas = [{nome: "maça", cor: "vermelha"} , {nome: "uva", cor:"roxo"}];
+//var idade = prompt("Qual a sua idade");
+//var count = 0;
+//var count;
+var d = new Date();
 
-//alert("Bem vindo " + nome + ", voce tem " + idade + " anos");
+/*
+alert("Bem vindo " + nome + ", voce tem " + idade + " anos");
 console.log(nome);
 console.log(idade + idade2);
 console.log(idade * idade2);
 console.log(frase.replace("Japão", "Brasil"));
 console.log(frase.toLowerCase);
 console.log(frase.toUpperCase);
-//alert(frase.replace("Japão", "Brasil"));
+lista.push("uva");
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+alert(lista[0]);
+alert(frase.replace("Japão", "Brasil"));
+console.log(fruta.nome);
+console.log(fruta.cor);
+console.log(frutas);
+*/
+
+/*
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade")
+};
+*/
+
+/*
+while (count < 5){
+    //console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+/*
+for(count = 0; count < 5; count++){
+    //alert(count);
+    console.log(count);
+};
+*/
+
+//alert(d.getMonth()+1);
+alert(d.getMinutes());
